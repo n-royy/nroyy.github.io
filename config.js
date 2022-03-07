a14fc5fc3e8368e46b53880517e8162ed2808f58
n-royy <roynguyen18496@gmail.com>
2022-03-08 00:25:07 +0700

    aniversary

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'He luu bé!',
+    introDesc: `3 năm trôi cái vèo nhờ, đã trãi qua biết bao nhiêu thứ cùng nhau, vui vì 2 đứa vẫn đồng hành cùng với nhau`,
+    btnIntro: 'Iu Nguyên!',
+    title: 'Bà yêu tui nhiều lắm chi nữa ._.',
+    desc: 'Không trả lời mà thoát ra là sủa gâu gâu =))',
+    btnYes: 'Iu Nam nhiều nhất ạ <33',
+    btnNo: 'Không, Ông mơ à???',
+    question: 'Nhiều như thế nào ạ :vvvv',
+    btnReply: 'Gửi cho nam kute <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Tui biết mà 🥰. Tui cũng yêu bà nhiều lắm, nhiều hơn những gì bà nghĩ 😘😘',
+    messDesc: 'Tối nay 8h tui qua đón nghen.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/boo.bipbom' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
